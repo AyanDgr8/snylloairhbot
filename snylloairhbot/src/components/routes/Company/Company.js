ecc0817ccc99c2144a222d270b18421d17cf09df
Ayan <ayandgr5@gmail.com>
2024-02-15 15:39:43 +0530

    second last changes

--- snylloairhbot/src/components/routes/Company/Company.js
+++ snylloairhbot/src/components/routes/Company/Company.js
@@ -22,10 +22,10 @@ const Company = () => {
             </LazyLoad>
             <div className='company-container-one-content'>
                 <div className='company-container-one-line1'>
-                Bringing the future of hyperbaric medicine to fruition
+                Innovating Hyperbaric Medicine
                 </div>
                 <div className='company-container-one-line2'>
-                    We are a company that manufactures and distributes medium pressure hyperbaric chambers that aims to develop hyperbaric medicine with high quality standards in equipment and services around the world.
+                We are a company specialized in the manufacture and distribution of medium pressure Hyperbaric Chambers that aims to develop Hyperbaric Medicine with high quality standards in equipment and services around the world.
                 </div>
             </div>
 
@@ -47,9 +47,10 @@ const Company = () => {
                         ABOUT US
                     </div> 
                     <div className='company-content-line2'>
-                        SnylloAir is the brand of hyperbaric chambers manufactured by Biobarica Medical Hyperbaric Solutions, the network of centers and multidisciplinary professionals that aims to develop hyperbaric medicine with high quality standards in equipment and services around the world.<br></br><br></br>
+                        In order to further develop Hyperbaric Medicine and expand the availability of treatment worldwide, SnylloAir developed the first medium-pressure Hyperbaric Chamber on the market.<br></br><br></br>
                         SnylloAir hyperbaric chambers are therapeutically efficient and safe medical devices that can be easily installed in any space that meets the requirements of medical activity.<br></br><br></br>
-                        For more information on our hyperbaric chambers, please contact us.
+                        SnylloAir provides a comprehensive and cost-effective treatment system that will allow you to enhance your professional practice with the objective of providing your patients with a safe and non-invasive therapy.<br></br><br></br>
+                        Join us into the future of Hyperbaric Medicine.
                     </div> 
                 
                 </div>
@@ -57,6 +58,163 @@ const Company = () => {
             </div>
 
         </section>
+
+        {/* *************************8 */}
+
+        <section className='company-container-three'>
+            <div className='mission-vision-box'>
+                <div className='mission-vision'>
+                    <div className='mission-vision-caption'>
+                            <img 
+                                src="/uploads/mission-icon.png"
+                                className=' mis-vis-icon'
+                                alt="mission-icon" 
+                            />
+                        <span className='mis-vis-heading'> Mission </span>
+                    </div>
+                    <p className='mis-vis-para'>
+                        To promote and disseminate Hyperbaric Oxygen Therapy among health professionals in order to make it a real possibility for all those patients who need it.
+                    </p>
+                </div>
+
+                <div className='mission-vision'>
+                    <div className='mission-vision-caption'>
+                            <img 
+                                src="/uploads/vision-icon.png"
+                                className='mis-vis-icon'
+                                alt="vision-icon" 
+                            />
+                        <span className='mis-vis-heading'> Vision </span>
+                    </div>
+                    <p className='mis-vis-para'>
+                        To be a leading specialist in Hyperbaric Oxygen Therapy and to provide all medical institutions with a treatment system of high therapeutic efficiency.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+
+        {/* ********* */}
+
+        <section className='company-container-four'>
+            <div className='company-four-heading'>Our Values</div>
+            
+            <div className="company-boxes">
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-one.png"
+                                className='company-one'
+                                alt="company-one"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                        Global vision
+                    </div>
+                    <div className="company-box-para">
+                        We work to expand the availability of Hyperbaric Oxygen Therapy in the world. We offer professionals high quality Hyperbaric Chambers to contribute to the treatment of their patients.
+                    </div>
+                </div>
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-two.png"
+                                className='company-two'
+                                alt="company-two"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                        Medical ethics
+                    </div>
+                    <div className="company-box-para">
+                        We act according to universal ethical and medical principles to guarantee patients a safe, non-invasive and beneficial treatment for different pathologies.
+                    </div>
+                </div>
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-three.png"
+                                className='company-three'
+                                alt="company-three"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                    Customer focus
+                    </div>
+                    <div className="company-box-para">
+                    We aim to understand our clients' needs and meet their expectations, build trust and add value to their professional practice.
+                    </div>
+                </div>
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-four.png"
+                                className='company-four'
+                                alt="company-four"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                    Results oriented
+                    </div>
+                    <div className="company-box-para">
+                    We incorporate Hyperbaric Chambers of high therapeutic efficiency and internationally certified. We support with scientific evidence the use of our medical devices in Hyperbaric Oxygen Therapy to guarantee optimal results.
+                    </div>
+                </div>
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-five.png"
+                                className='company-five'
+                                alt="company-five"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                    Competitiveness
+                    </div>
+                    <div className="company-box-para">
+                    We seek to improve ourselves day by day to achieve the best results for health professionals and patients. Always with a clear objective of being leaders in high quality Hyperbaric Therapy.
+                    </div>
+                </div>
+
+                <div className="company-box">
+                    <div className="company-icon">
+                        <LazyLoad>
+                            <img 
+                                src="/uploads/company-six.png"
+                                className='company-six'
+                                alt="company-six"
+                            />
+                        </LazyLoad>
+                    </div>
+                    <div className="company-box-heading">
+                    Teamwork
+                    </div>
+                    <div className="company-box-para">
+                    Our work philosophy and our commitment are fundamental. We promote harmony, organization and cooperation among each member of the Biobarica network to achieve our common goals.
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+
         <Footer />
         <LowerFooter />
             
--- snylloairhbot/src/components/routes/Oxygen/Aging/Aging.js
+++ snylloairhbot/src/components/routes/Oxygen/Aging/Aging.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import LazyLoad from 'react-lazyload';
+import FormContact from '../../LandingPage/FormContact/FormContact';
 import Header from '../../LandingPage/Header/Header';
 import Footer from '../../LandingPage/Footer/Footer';
 import LowerFooter from '../../LandingPage/LowerFooter/LowerFooter';
@@ -41,13 +42,223 @@ const Aging = () =>{
 
             <section className="aging-container-two">
 
-                <div></div>
-                <div></div>
+                <div className="aging-two-left">
+                    <img 
+                    src="/uploads/otto.png"
+                    className="otto"
+                    alt="otto" 
+                    />
+                    <div className="otto-heading">
+                        Dr. Otto Warburg, Nobel Prize winner in medicine (1931)
+                    </div>
+                    <div className="otto-line">
+                        "The fundamental cause of all degenerative disease is a condition called hypoxia, oxygen deprival at cellular level."
+                    </div>
+
+                </div>
+                <div className="aging-two-right">
+                    <div className="aging-two-content">
+                        <div className="aging-two-heading">
+                        Master your breath, master your health
+                        </div>
+                        <div className="aging-two-para">
+                        Oxygen controls most of the body’s processes, including the immune system. Lack of oxygen can lead to a vulnerable immune response, increased fatigue and eventually hypoxia, oxygen starvation in the cells which is considered the cause of many degenerative diseases.
+                        </div>
+                    </div>
+                </div>
 
 
             </section>
 
+            {/* ****** */}
 
+            <section className="aging-container-three">
+                <div className="aging-three-left">
+                    <div className="aging-three-content">
+                        <div className="aging-three-line1">
+                        Pure oxygen to stimulate the natural healing process
+                        </div>
+                        <div className="aging-three-line2">
+                        Hyperbaric oxygen therapy sessions are comfortable and relaxing and all you have to do is breathe pure oxygen inside a chamber with increased atmospheric pressure that enhances the distribution of oxygen through blood capillaries, through tissues, organs, and the brain. The treatment is non-invasive and fit for mostly everyone, with a few exceptions.
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <button className='butt-hbot buttt'>Read more about potential risks </button>
+                    </div>
+                </div>
+                <div className="aging-three-right">
+                    <LazyLoad>
+                        <img 
+                        src="/uploads/aging-boy11.png"
+                        className="aging-boy11"
+                        alt="aging-boy11"
+                        />
+                    </LazyLoad>
+                </div>
+
+            </section>
+
+
+
+            {/* ************** */}
+
+
+            <section className="aging-container-four">
+                <div className="aging-four-left">
+                    <LazyLoad>
+                        <img 
+                            src="/uploads/snylloair-capsule.png"
+                            className="snylloair-capsulee"
+                            alt="snylloair-capsulee" 
+                        />
+                    </LazyLoad>
+
+                    <div>
+                        <button className='butt-hbot butty'>Step inside our hyperbaric chambers </button>
+                    </div>
+                </div>
+
+                <div className="aging-four-right">
+                    <div className="aging-four-content">
+                        <div className="aging-four-line1">
+                            Hyperbaric Oxygen Therapy can be beneficial for:
+                        </div>
+                        <div className="aging-four-line2">
+                            Stimulate the natural recovery process and immunity.<br></br><br></br>
+                            Boost vitality, heighten concentration and memory.<br></br><br></br>
+                            Reduce fatigue and improve energy levels, stamina and endurance.<br></br><br></br>
+                            Improve cell rejuvenation and help with anti-aging strategies.<br></br><br></br>
+                            Remove the sensation of brain fog and jet lag.<br></br><br></br>
+                            Eliminates toxins and debris.<br></br><br></br>
+                            Reactivate skin cells and improve blood circulation 
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+            {/* ***************** */}
+
+
+            
+
+            <section className="aging-container-five">
+                <LazyLoad>
+                    <img 
+                    src="/uploads/blood.png"
+                    className="shiny"
+                    alt="shiny" 
+                    />
+                </LazyLoad>
+                <div className="aging-five-content">
+                    <div className="aging-five-line1">
+                        Hyperbaric Oxygen Therapy is as natural as breathing
+                    </div>
+                    <div className="aging-five-line2">
+                        One of the most efficient natural methods to increase both physical and cognitive performance.
+                    </div>
+                </div>
+            </section>
+
+
+            {/* ****************** */}
+
+            <section className="aging-container-six">
+
+                <div className="aging-six-para">
+                    <div className="aging-six-line1">
+                        The beneficial role of HBOT for the elderly
+                    </div>
+                    <div className="aging-six-line2">
+                        Aging is not a disease per se, but certain diseases are associated with aging. Hyperbaric oxygenation (HBO) has an important role. Multiple factors, including the inborn aging process, genetic defects, and environmental agents, participate in the aging process. There are more than 300 theories to explain the aging phenomenon. The most well-known of these states is that aging may be due to deleterious, irreversible changes produced by free radical reactions.
+                    </div>
+                </div>
+
+                <div className="aging-boxes">
+
+                    <div className="aging-box">
+                        <div className="aging-box-line1">
+                        Free Radicals and Aging
+                        </div>
+                        <div className="aging-box-line3">
+                            The free radical theory of aging postulates that a single common process, modified by genetic and environmental factors, is responsible for the aging and death of all living cells.
+                        </div>
+                        <div className="aging-box-line2">
+                        The free radical theory of aging, postulated by Denham Harman in 1954, remains the most popular and widely tested theory of aging. It is based on the chemical nature and ubiquitous presence of free radicals.<br></br><br></br>
+                        Despite the actions of antioxidant nutrients, some oxidative damage will occur, and accumulation of this damage throughout life is believed to be a major contributing factor to aging and disease. Oxidative stress and production of free radicals tend to increase with aging,whereas the body’s natural antioxidant defenses decline.
+                        </div>
+                    </div>
+
+                    <div className="aging-box">
+                    <LazyLoad>
+                        <img 
+                        src="/uploads/aging-boy22.png"
+                        className="aging-boy22"
+                        alt="aging-boy22" 
+                        />
+                    </LazyLoad>
+                    </div>
+                    
+                    <div className="aging-box ">
+                        <div className="aging-box-line1">
+                        Oxygen therapy to help elderly persons
+                        </div>
+                        <div className="aging-box-line3">
+                        Helping elderly people to regain health can be a real challenge
+                        </div>
+                        <div className="aging-box-line2">
+                        The capacity for physical exercise is reduced in these group of people. HBO may not increase the physical performance in healthy adults, but it can help to regain the physical strengh for elderly people. Physical therapy can be carried out in a multi-place hyperbaric chamber.<br></br><br></br>
+                        The main object of HBO is to help counteract the deprival of oxygen and help improve the metabolism. These two disturbances, also affect the brain, which constitutes a major portion of the problems for eledery persons. Eldery users might experience positive effects after using hyperbaric oxygen therapy in this regard.
+                        </div>
+                    </div>
+
+                    <div className="aging-box">
+                        <div className="aging-box-line1">
+                        Hypoxia is central to decreasing mental function with aging
+                        </div>
+                        <div className="aging-box-line3">
+                        The deprival of oxygen accompanies the aging process and hyperbaric oxygen therapy can counteract this.
+                        </div>
+                        <div className="aging-box-line2">
+                        Aging can be viewed as a manifestation of hypoxia. The aging process involves a diminished capacity of the central nervous system to use oxygen. The decline of mental function resulting from aging is similar to the decline resulting from hypoxia. Although there is impairment of functional activity with hypoxia in aging, the metabolic capacity of the brain is not totally exhausted.
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
+
+            {/* ************* */}
+
+            <section className="aging-container-seven">
+                <div className="aging-seven-left">
+                    <LazyLoad>
+                        <img 
+                        src="/uploads/aging-boy33.png"
+                        className="aging-boy33"
+                        alt="aging-boy33" 
+                        />
+                    </LazyLoad>
+                </div>
+                <div className="aging-seven-right">
+                    <div className="aging-seven-content">
+                        <div className="aging-seven-line1">
+                        HBO in the Management of Decline of Mental Function with Aging
+                        </div>
+                        <div className="aging-seven-line2">
+                        HBO is part of a multidisciplinary approach to the persons with decline of mental function with aging. It works best combined with brain jogging (mental exercises), physical exercise, and nootropic substances (to improve cerebral metabolism).<br></br><br></br>
+                        HBO can improve the overall mental functions, as well as metabolic disturbances associated with a lack of oxygen for elderly persons .<br></br><br></br>
+                        HBO is generally considered to be safe in the aged persons unless they have a medical contraindication.
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+
+            <FormContact/>
             <Footer />
             <LowerFooter />
         </div>
